11d6a446864003dcbdb820d23b0eb4a8997794e3
KadVenku <venkukad@gmail.com>
2018-07-26 17:01:42 +0000

    Base init

---
+++ eaw-texteditor/client/ui/main/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace eaw_texteditor.client.ui.main
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
